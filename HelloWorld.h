78153b7b8b9772c34dd0a502f8893c87ae91a892
KIIIKIII <34474819+KIIIKIII@users.noreply.github.com>
2018-03-13 12:39:24 +0800

    Update HelloWorld.h

--- HelloWorld.h
+++ HelloWorld.h
@@ -1,6 +1,11 @@
 #ifndef _HELLO_WORLD_
 #define _HELLO_WORLD_
 class HelloWorld {
-  
+  public:
+    void helloworld() {
+      cout << c << endl;
+    }
+  private:
+    char c[] = {"HelloWorld!"};
 };
 #endif
