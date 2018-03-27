016cf5395d6a73ad4086a8c13a616287a0dc7b9d
KIIIKIII <34474819+KIIIKIII@users.noreply.github.com>
2018-03-27 12:10:38 +0800

    Update Prime.cpp

--- Prime.cpp
+++ Prime.cpp
@@ -1,23 +1,86 @@
+#pragma once
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+using namespace std;
+
 class Prime {
 public:
-  Prime(int num);
-  ~Prime();
-
-  bool isPrime();
-
-  Prime sumBit();
-  Prime multiBit();
-  Prime sqaureSumBit();
+	Prime();
+	Prime(int n);
+	int sum_bit(int num);
+	int multi_bit(int num);
+	int square_sum_bit(int num);
+	bool isprime(int num);
+	void print(int num);
+	~Prime();
 private:
-  const int mNum;
+	int num;
 };
 
+#include "Prime.h"
+
+
+
+Prime::Prime() {
+}
+
+Prime::Prime(int n) {
+	int num = n;
+}
+
+int Prime::sum_bit(int num) {
+	int q = num / 1000; //千位
+	int w = num % 1000 / 100; //百位
+	int e = num % 100 / 10; //十位
+	int r = num % 10; //个位
+	return q + w + e + r;
+	return 0;
+}
+
+int Prime::multi_bit(int num)
+{
+	int q = num / 1000; //千位
+	int w = num % 1000 / 100; //百位
+	int e = num % 100 / 10; //十位
+	int r = num % 10; //个位
+	return q * w * e * r;
+}
+
+int Prime::square_sum_bit(int num) {
+	int q = num / 1000; //千位
+	int w = num % 1000 / 100; //百位
+	int e = num % 100 / 10; //十位
+	int r = num % 10; //个位
+	return q * q + w * w + e * e + r * r;
+}
+
+bool Prime::isprime(int num) {
+	for (int i = 2; i <= sqrt(num); i++) {
+		if (num % i == 0) return false;
+	}
+	return true;
+}
+
+void Prime::print(int num) {
+	if (isprime(num) && isprime(sum_bit(num)) && isprime(multi_bit(num)) && isprime(square_sum_bit(num)))
+		cout << num << " is super prime." << endl;
+	else cout << num << " is not super prime." << endl;
+}
+
+Prime::~Prime() {
+
+}
+
 int main() {
-  Prime pm(113);
-  Prime sb = pm.sumBit();
-  Prime mb = pm.multiBit();
-  Prime ssb = pm.sqaureSumBit();
-  if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
-  return 0;
+	int n;
+	cin >> n;
+	Prime prime(n);
+	prime.print(n);
+
+	return 0;
 }
