c0fa4e97978b51d0ec770904903f372c73fb4bd5
KIIIKIII <34474819+KIIIKIII@users.noreply.github.com>
2018-06-18 13:50:38 +0800

    Update units.h

--- units.h
+++ units.h
@@ -12,7 +12,12 @@ const int INT = 100;
 
 class unit {
 public:
-	unit() {}
+	unit() {
+		memset(unit1, 0, INT);
+		memset(unit2, 0, INT);
+		x = 0;
+		y = 0;
+	}
 	~unit() {}
 	void input() {
 		cout << "请输入原单位：";
@@ -37,8 +42,8 @@ char wei[INT][5] = {"ug", "mg", "g", "kg", "t", "lb", "gr", "oz", "斤", "两", 
 double w[INT] = {1000000, 1000, 1, 0.001, 0.000001, 0.0022046, 15.4323584, 0.035274, 0.002, 0.02, 0.00001, 5, 0.5643834, 0.00002, 0.2};
 class weight :public unit {
 public:
-	void convert() {
-		int flag1 = 0, flag2 = 0;
+	int convert() {
+		int f = 0, flag1 = -1, flag2 = -1;
 		if (isalnum(unit1[0])) {
 			for (int i = 0; i < strlen(unit1); i++) {
 				unit1[i] = tolower(unit1[i]);
@@ -61,7 +66,12 @@ public:
 				break;
 			}
 		}
-		y = x / w[flag1] * w[flag2];
+		if (flag1 < 0 || flag2 < 0) {
+			cout << "输入单位不存在" << endl;
+			f = 1;
+		}
+		else y = x / w[flag1] * w[flag2];
+		return f;
 	}
 };
 
@@ -69,8 +79,8 @@ char pre[INT][5] = {"pa", "mmhg", "bar", "kpa", "psi", "atm", "mpa", "psf", "hpa
 double p[INT] = {1, 0.0075006, 0.00001, 0.001, 0.000145, 9.8692e-6, 1e-6, 0.0208854, 0.01, 0.0002953, 0.01};
 class pressure :public unit {
 public:
-	void convert() {
-		int flag1 = 0, flag2 = 0;
+	int convert() {
+		int f = 0, flag1 = -1, flag2 = -1;
 		if (isalnum(unit1[0])) {
 			for (int i = 0; i < strlen(unit1); i++) {
 				unit1[i] = tolower(unit1[i]);
@@ -93,7 +103,12 @@ public:
 				break;
 			}
 		}
-		y = x / p[flag1] * p[flag2];
+		if (flag1 < 0 || flag2 < 0) {
+			cout << "输入单位不存在" << endl;
+			f = 1;
+		}
+		else y = x / p[flag1] * p[flag2];
+		return f;
 	}
 };
 
@@ -101,8 +116,8 @@ char len[INT][5] = {"m", "cm", "km", "mm", "dm", "um", "nm", "pm", "ly", "au", "
 double l[INT] = {1, 100, 0.001, 1000, 10, 1e6, 1e9, 1e12, 1.057e-16, 6.6846e-12, 39.3700787, 0.0006214, 3.2808399, 3, 1.0936133, 0.00054, 0.5468066, 0.004971, 0.002, 0.3, 30, 300, 3000, 30000};
 class length :public unit {
 public:
-	void convert() {
-		int flag1 = 0, flag2 = 0;
+	int convert() {
+		int flag1 = -1, flag2 = -1, f = 0;
 		if (isalnum(unit1[0])) {
 			for (int i = 0; i < strlen(unit1); i++) {
 				unit1[i] = tolower(unit1[i]);
@@ -125,6 +140,11 @@ public:
 				break;
 			}
 		}
-		y = x / l[flag1] * l[flag2];
+		if (flag1 < 0 || flag2 < 0) {
+			cout << "输入单位不存在" << endl;
+			f = 1;
+		}
+		else y = x / l[flag1] * l[flag2];
+		return f;
 	}
 };
