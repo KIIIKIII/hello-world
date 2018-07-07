317ea57f5aaeea1b3b7524013a45af111775f9db
KIIIKIII <34474819+KIIIKIII@users.noreply.github.com>
2018-07-07 11:48:20 +0800

    线段树

--- Count the Colors.cpp
+++ Count the Colors.cpp
@@ -4,31 +4,31 @@
 using namespace std;
 const int MAXN = 8005;
 int n, col[MAXN << 2], vis[MAXN << 2], ans[MAXN << 2];
+//记录染色、数颜色数目、记录答案
 
 void update(int rt, int left, int right, int l, int r, int data) {
-	if (l <= left && right <= r) {
+	//根、最左、最右、此处染色最左、此处染色最右，颜色
+	if (l <= left && right <= r) {//染色范围大于根
 		col[rt] = data;
 		return;
 	}
-	if (col[rt] == data) return;
-	if (col[rt] != -1) {
-		if (col[rt] != -1) {
-			col[rt << 1] = col[rt << 1 | 1] = col[rt];
-			col[rt] = -1;
-		}
+	if (col[rt] == data) return;//根已经染好此色
+	if (col[rt] != -1) {//根已经染了其他色
+		col[rt << 1] = col[rt << 1 | 1] = col[rt];//生成左右儿子染上根色
+		col[rt] = -1;//根设置为未染色
 	}
-	int m = (left + right) / 2;
+	int m = (left + right) / 2;//给儿子染新色
 	if (l <= m)update(rt << 1, left, m, l, r, data);
 	if (r > m)update(rt << 1 | 1, m + 1, right, l, r, data);
 }
 void query(int rt, int left, int right) {
-	if (col[rt] >= 0) {
+	if (col[rt] >= 0) {//根已经染色，直接统计根部
 		for (int i = left; i <= right; ++i)
-			vis[i] = col[rt];
+			vis[i] = col[rt];//统计
 		return;
 	}
-	if (left != right && col[rt] == -1) {
-		int m = (left + right) / 2;
+	if (left != right && col[rt] == -1) {//根未染色
+		int m = (left + right) / 2;//向儿子统计
 		query(rt << 1, left, m);
 		query(rt << 1 | 1, m + 1, right);
 	}
@@ -46,7 +46,7 @@ int main() {
 		memset(vis, -1, sizeof(vis));
 		query(1, 1, MAXN);
 		memset(ans, 0, sizeof(ans));
-		for (int i = 1; i < MAXN; ) {
+		for (int i = 1; i < MAXN; ) {//统计答案
 			int color = vis[i], j = i + 1;
 			if (color == -1) { i++; continue; }
 			while (vis[j] != -1 && vis[j] == color && j < MAXN) ++j;
